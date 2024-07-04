674d9d6a5786cfc6f18eadcbb5bd4a7731b8f8cc
nathan <nathan@appflowy.io>
2024-07-04 10:47:59 +0800

    chore: fmt

--- appflowy-local-ai/src/state.rs
+++ appflowy-local-ai/src/state.rs
@@ -4,34 +4,34 @@ use appflowy_plugin::util::get_operating_system;
 
 #[derive(Debug, Clone)]
 pub enum LLMState {
-    Uninitialized,
-    Loading,
-    Ready { plugin_id: PluginId },
+  Uninitialized,
+  Loading,
+  Ready { plugin_id: PluginId },
 }
 
 impl LLMState {
-    pub(crate) fn plugin_id(&self) -> anyhow::Result<PluginId> {
-        match self {
-            LLMState::Ready { plugin_id } => Ok(*plugin_id),
-            _ => Err(anyhow!("chat plugin is not ready")),
-        }
+  pub(crate) fn plugin_id(&self) -> anyhow::Result<PluginId> {
+    match self {
+      LLMState::Ready { plugin_id } => Ok(*plugin_id),
+      _ => Err(anyhow!("chat plugin is not ready")),
     }
+  }
 
-    pub(crate) fn is_loading(&self) -> bool {
-        matches!(self, LLMState::Loading)
-    }
+  pub(crate) fn is_loading(&self) -> bool {
+    matches!(self, LLMState::Loading)
+  }
 
-    #[allow(dead_code)]
-    fn is_uninitialized(&self) -> bool {
-        matches!(self, LLMState::Uninitialized)
-    }
+  #[allow(dead_code)]
+  fn is_uninitialized(&self) -> bool {
+    matches!(self, LLMState::Uninitialized)
+  }
 
-    pub(crate) fn is_ready(&self) -> bool {
-        let system = get_operating_system();
-        if system.is_desktop() {
-            matches!(self, LLMState::Ready { .. })
-        } else {
-            false
-        }
+  pub(crate) fn is_ready(&self) -> bool {
+    let system = get_operating_system();
+    if system.is_desktop() {
+      matches!(self, LLMState::Ready { .. })
+    } else {
+      false
     }
+  }
 }
