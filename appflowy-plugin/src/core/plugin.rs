0a31110905d42e7807dc821a3ee3b2f86bf73e14
nathan <nathan@appflowy.io>
2024-07-15 10:48:07 +0800

    chore: remove unix ref

--- appflowy-plugin/src/core/plugin.rs
+++ appflowy-plugin/src/core/plugin.rs
@@ -9,7 +9,6 @@ use anyhow::anyhow;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value as JsonValue};
 use std::io::BufReader;
-use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
 use std::process::{Child, Command, Stdio};
 use std::sync::Arc;
@@ -276,6 +275,8 @@ pub(crate) async fn start_plugin_process(
 
 #[cfg(unix)]
 async fn ensure_executable(exec_path: &Path) -> Result<(), anyhow::Error> {
+  use std::os::unix::fs::PermissionsExt;
+
   let metadata = tokio::fs::metadata(exec_path).await?;
   let mut permissions = metadata.permissions();
   permissions.set_mode(permissions.mode() | 0o755);
